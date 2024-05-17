3258d794feffd32830ce3492c3f221a3296dae2b
stefanoracke <stefanoracke@gmail.com>
2024-05-16 23:23:49 -0300

    cambiado el texto

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,7 @@ export default function Home() {
     requestAnimationFrame(raf)
   }, [])
 
-  const text = "Desplazate hacia abajo y mirá algo increíble. Mientras te movés, los párrafos se van a desvanecer suavemente, haciendo que la info fluya de una manera muy copada. Este efecto le da un toque de estilo y mantiene las cosas interesantes. Seguí desplazándote y sorprendete con el resultado."
+  const text = "Desplazate hacia abajo y mirá algo increíble. Mientras te movés, los párrafos se van a desvanecer suavemente, haciendo que la info fluya de una manera muy copada. Este efecto le da un toque de estilo y mantiene el interes del usuario. Seguí desplazándote y sorprendete con el resultado."
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
